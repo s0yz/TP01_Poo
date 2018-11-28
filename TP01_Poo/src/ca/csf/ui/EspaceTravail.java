c18e8dcc9e835420edbddb5b1920f6ca7e935dcd
CegepDFC <oehaut@hotmail.com>
2018-11-28 17:19:29 -0500

    Espace de travail

--- TP01_Poo/src/ca/csf/ui/EspaceTravail.java
+++ TP01_Poo/src/ca/csf/ui/EspaceTravail.java
@@ -1,30 +1,43 @@
 package ca.csf.ui;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
-
 import ca.csf.formes.ElementGraphique;
+import ca.csf.formes.Formes;
+import ca.csf.formes.Rectangle;
 import ca.csf.modele.EcouteurModeleGraphique;
 import ca.csf.modele.ModeleElementGraphique;
+import ca.csf.modele.ModeleGraphiques;
 
 /**
  * 
- * 
+
  * @author
  */
 public class EspaceTravail extends JPanel implements EcouteurModeleGraphique {
 	
 	private static final long serialVersionUID = -7570189304007187337L;
+	private ModeleElementGraphique modeleGraphique;
+	private ElementGraphique rectangle;
 	
-	public EspaceTravail(ModeleElementGraphique p_Modele) {
-		// TODO
+	public EspaceTravail(ModeleElementGraphique p_Modele, Integer p_largeur, Integer p_hauteur) {
+		this.modeleGraphique = p_Modele;
+		this.modeleGraphique.ajouterEcouteur(this);
+		this.rectangle = new Rectangle(0, 0, p_largeur, p_hauteur);
 	}
-
+	
+	@Override
+	protected void paintComponent(Graphics arg0) {
+		// TODO Auto-generated method stub
+		super.paintComponent(arg0);
+		
+	} 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public void reagirModifications() {
-		// TODO
+		this.repaint();
 	}
 
 	/**
@@ -32,6 +45,6 @@ public class EspaceTravail extends JPanel implements EcouteurModeleGraphique {
 	 */
 	@Override
 	public void reagirNouvelElement(ElementGraphique p_Element) {
-		// TODO
+		this.repaint(p_Element.getPosition().x, p_Element.getPosition().y, p_Element.getLargeur(), p;
 	}
 }
