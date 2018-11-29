f32da396a2610e594b1a6ccec9ddbd3a1dd9e274
Cedric Mariage <soyz@hotmail.ca>
2018-11-29 02:00:14 -0500

    Update FenetrePrincipale.java

--- TP01_Poo/src/ca/csf/ui/FenetrePrincipale.java
+++ TP01_Poo/src/ca/csf/ui/FenetrePrincipale.java
@@ -1,5 +1,15 @@
 package ca.csf.ui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import ca.csf.modele.ModeleGraphiques;
 
 /**
  * @author Cedric Mariage
@@ -8,15 +18,43 @@ public class FenetrePrincipale extends JFrame {
 
 	private static final long serialVersionUID = -4586998190495167383L;
 	
+	private ModeleGraphiques m_Modele;
+	private EspaceTravail m_Espace;
+	
 	public FenetrePrincipale() {
 		super("TP01 - Poo");
+		this.parametrer();
+		this.initialiserComposant();
+		super.pack();
 	}
 	
 	public void parametrer() {
-		
+		super.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		super.setLayout(new BorderLayout());
+		super.setLocationRelativeTo(null);
 	}
 	
 	public void initialiserComposant() {
-		
+		//
+		// center
+		JPanel center = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		center.setOpaque(true);
+		center.setBackground(Color.gray);
+		super.add(center, BorderLayout.CENTER);
+		//
+		// m_Modele
+		this.m_Modele = new ModeleGraphiques();
+		//
+		// m_Espace
+		this.m_Espace = new EspaceTravail(this.m_Modele, 640, 360);
+		center.add(this.m_Espace);
+		//
+		//
+		super.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent p_e) {
+				FenetrePrincipale.this.dispose();
+			}
+		});
 	}
 }
