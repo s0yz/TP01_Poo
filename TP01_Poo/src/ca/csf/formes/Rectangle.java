7966a046b9ecfb0b97607a823c7ac972b52b5c0e
Cedric Mariage <soyz@hotmail.ca>
2018-11-28 21:16:13 -0500

    Update Rectangle.java

--- TP01_Poo/src/ca/csf/formes/Rectangle.java
+++ TP01_Poo/src/ca/csf/formes/Rectangle.java
@@ -1,80 +1,200 @@
 package ca.csf.formes;
 
-import java.awt.Color;
 import java.awt.Point;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.PathIterator;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
-public class Rectangle implements ElementGraphique {
+/**
+ * 
+ * @author Cedric Mariage
+ */
+public class Rectangle extends Forme {
 	
-	private Integer x;
-	private Integer y;
-	private Integer largeur;
-	private Integer Hauteur;
-
-	public Rectangle (Integer p_x, Integer p_y, Integer p_Largeur, Integer p_Hauteur) {
-		this.x = p_x;
-		this.y = p_y;
-		this.largeur = p_Largeur;
-		this.Hauteur = p_Hauteur;
+	/**
+	 * 
+	 */
+	private java.awt.Rectangle m_Rectangle;
+	
+	/**
+	 * 
+	 */
+	Rectangle() {
+		this(new Point(0, 0), 0, 0);
 	}
 	
+	public Rectangle(int p_X, int p_Y, int p_Largeur, int p_Hauteur) {
+		super("Rectangle");
+		this.m_Rectangle = new java.awt.Rectangle(p_X, p_Y, p_Largeur, p_Hauteur);
+	}
+	
+	/**
+	 * @param m_Position
+	 * @param p_Largeur
+	 * @param p_Hauteur
+	 */
+	Rectangle(Point m_Position, int p_Largeur, int p_Hauteur) {
+		this(m_Position.x, m_Position.y, p_Largeur, p_Hauteur);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getX() {
+		return this.m_Rectangle.x;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getY() {
+		return this.m_Rectangle.y;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Point getPosition() {
-		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int getLargeur() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.m_Rectangle.width;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int getHauteur() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.m_Rectangle.height;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	public int getCouleur() {
-		// TODO Auto-generated method stub
-		return 0;
+	public java.awt.Rectangle getBounds() {
+		return this.m_Rectangle.getBounds();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	public String getNom() {
-		// TODO Auto-generated method stub
+	public Rectangle2D getBounds2D() {
 		return null;
 	}
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean contains(double p_x, double p_y) {
+		return this.m_Rectangle.contains(p_x, p_y);
+	}
 
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean contains(Point2D p_p) {
+		return this.m_Rectangle.contains(p_p);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean intersects(double p_x, double p_y, double p_w, double p_h) {
+		return this.m_Rectangle.intersects(p_x, p_y, p_w, p_h);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean intersects(Rectangle2D p_r) {
+		return this.m_Rectangle.intersects(p_r);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean contains(double p_x, double p_y, double p_w, double p_h) {
+		return this.m_Rectangle.contains(p_x, p_y, p_w, p_h);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean contains(Rectangle2D p_r) {
+		return this.m_Rectangle.contains(p_r);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public PathIterator getPathIterator(AffineTransform p_at) {
+		return this.m_Rectangle.getPathIterator(p_at);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public PathIterator getPathIterator(AffineTransform p_at, double p_flatness) {
+		return m_Rectangle.getPathIterator(p_at, p_flatness);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setPosition(Point p_Position) {
-		// TODO Auto-generated method stub
-
+		this.m_Rectangle.setLocation(p_Position);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setPosition(int p_X, int p_Y) {
-		// TODO Auto-generated method stub
-
+		this.m_Rectangle.x = p_X;
+		this.m_Rectangle.y = p_Y;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setLargeur(int p_Largeur) {
-		// TODO Auto-generated method stub
-
+		this.m_Rectangle.width = p_Largeur;
 	}
-
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setHauteur(int p_Hauteur) {
-		// TODO Auto-generated method stub
-
+		this.m_Rectangle.height = p_Hauteur;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	public void setCouleur(Color p_Couleur) {
-		// TODO Auto-generated method stub
-
+	public boolean contient(Point p_Point) {
+		return this.m_Rectangle.contains(p_Point);
 	}
-
 }
