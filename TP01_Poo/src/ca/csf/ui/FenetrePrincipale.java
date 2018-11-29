26aa39cb9bdc30683b87d0d5877ca67abe82122f
Cedric Mariage <soyz@hotmail.ca>
2018-11-28 20:54:49 -0500

    Cleaning the repo (had weird stuff)

--- TP01_Poo/src/ca/csf/ui/FenetrePrincipale.java
+++ TP01_Poo/src/ca/csf/ui/FenetrePrincipale.java
@@ -8,4 +8,15 @@ public class FenetrePrincipale extends JFrame {
 
 	private static final long serialVersionUID = -4586998190495167383L;
 	
+	public FenetrePrincipale() {
+		super("TP01 - Poo");
+	}
+	
+	public void parametrer() {
+		
+	}
+	
+	public void initialiserComposant() {
+		
+	}
 }
