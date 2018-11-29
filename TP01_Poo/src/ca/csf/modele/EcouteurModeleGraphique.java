eb0deecf3fa8d19aeb1d27d411441fcdacc1524e
Cedric Mariage <soyz@hotmail.ca>
2018-11-29 02:02:38 -0500

    Update EcouteurModeleGraphique.java

--- TP01_Poo/src/ca/csf/modele/EcouteurModeleGraphique.java
+++ TP01_Poo/src/ca/csf/modele/EcouteurModeleGraphique.java
@@ -3,18 +3,21 @@ package ca.csf.modele;
 import ca.csf.formes.ElementGraphique;
 
 /**
+ * Interface pour des ecouteurs de {@code ModeleElementGraphique}.
  * 
- * 
- * @author
+ * @author Cedric Mariage
  */
 public interface EcouteurModeleGraphique {
+	
 	/**
-	 * 
+	 * Invoqué par le modèle écoutée lors de changements majeurs.
 	 */
 	void reagirModifications();
 	
 	/**
+	 * Invoqué par le modèle écoutée lors de changements mineurs.
 	 * 
+	 * @param p_Element Element ayant été modifié.
 	 */
-	void reagirNouvelElement(ElementGraphique p_Element);
+	void reagirModifications(ElementGraphique p_Element);
 }
