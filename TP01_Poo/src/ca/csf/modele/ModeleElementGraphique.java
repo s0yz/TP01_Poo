437d06cc5a525cee62d1ce2e23a0149b3adc82eb
Cedric Mariage <soyz@hotmail.ca>
2018-11-29 02:02:43 -0500

    Update ModeleElementGraphique.java

--- TP01_Poo/src/ca/csf/modele/ModeleElementGraphique.java
+++ TP01_Poo/src/ca/csf/modele/ModeleElementGraphique.java
@@ -5,9 +5,35 @@ import ca.csf.formes.ElementGraphique;
 /**
  * 
  * 
- * @author
+ * @author Cedric Mariage
  */
 public interface ModeleElementGraphique extends Iterable<ElementGraphique> {
-	void ajouterEcouteur(EcouteurModeleGraphique p_Ecouteur);
 	
+	/**
+	 * Pour ajouter un élément.
+	 * 
+	 * @param p_Element l'élément à ajouter.
+	 */
+	void ajouter(ElementGraphique p_Element);
+
+	/**
+	 * Pour retirer un élément.
+	 * 
+	 * @param p_Element l'élément à retirer.
+	 */
+	void retirer(ElementGraphique p_Element);
+	
+	/**
+	 * Pour ajouter un écouteur.
+	 * 
+	 * @param p_Ecouteur l'écouteur à ajouter.
+	 */
+	void ajouterEcouteur(EcouteurModeleGraphique p_Ecouteur);
+
+	/**
+	 * Pour retirer un écouteur.
+	 * 
+	 * @param p_Ecouteur l'écouteur à retirer.
+	 */
+	void removeEcouteur(EcouteurModeleGraphique p_Ecouteur);
 }
