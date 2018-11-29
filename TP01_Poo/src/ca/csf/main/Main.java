a5c92b93e20e14a1898fcb352becd8685aab4ce6
Cedric Mariage <soyz@hotmail.ca>
2018-11-29 02:00:06 -0500

    Create Main.java

---
+++ TP01_Poo/src/ca/csf/main/Main.java
@@ -0,0 +1,15 @@
+package ca.csf.main;
+
+import ca.csf.ui.FenetrePrincipale;
+
+/**
+ * @author Cedric Mariage
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		FenetrePrincipale fenetre = new FenetrePrincipale();
+		fenetre.setVisible(true);
+	}
+}
